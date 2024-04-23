7447acf5a61553879d06da4d18e8fdc215594ade
mikewang122 <104990807+mikewang122@users.noreply.github.com>
2024-04-22 22:39:27 -0700

    Comment Item display

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -21,6 +21,9 @@ export const UPDATE_LIKES = 'UPDATE_LIKES';
 export const DELETE_POST = 'DELETE_POST';
 export const ADD_POST = 'ADD_POST';
 export const GET_POST = 'GET_POST';
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+
 // Compare this snippet from client/src/actions/alert.js:
 // import uuid from 'uuid';
 // import { SET_ALERT, REMOVE_ALERT } from './types';
--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,32 +1,47 @@
-import React, {useEffect, Fragment} from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { getPost } from '../../actions/post'
-import Spinner from '../layout/spinner'
-import PostItem from '../posts/PostItem'
-import { Link } from 'react-router-dom'
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Spinner from '../layout/spinner';
+import PostItem from '../posts/PostItem';
+import CommentForm from '../post/CommentForm';
+import CommentItem from '../post/CommentItem';
+import { getPost } from '../../actions/post';
 
-const Post = ({ getPost, post: {post, loading}, match }) => {
+const Post = ({ getPost, post: { post, loading }, match }) => {
+    useEffect(() => {
+        getPost(match.params.id);
+    }, [getPost]);
 
-  useEffect(() => {
-    getPost(match.params.id);
-  }, [getPost])
-
-
-  return loading || post === null ? (<Spinner/>) : (<Fragment>
-    <Link to = '/posts' className = 'btn'>
-      Back to Posts
-    </Link>
-    <PostItem post = {post} showActions = {false}/>
-  </Fragment>);
-}
+    return loading || post === null ? (
+        <Spinner />
+    ) : (
+        <Fragment>
+            <Link to='/posts' className='btn'>
+                Back To Posts
+            </Link>
+            <PostItem post={post} showActions={false} />
+            <CommentForm postId={post._id} />
+            <div className='comments'>
+                {post.comments.map(comment => (
+                    <CommentItem
+                        key={comment._id}
+                        comment={comment}
+                        postId={post._id}
+                    />
+                ))}
+            </div>
+        </Fragment>
+    );
+};
 
 Post.propTypes = {
-  getPost: PropTypes.func.isRequired,
-  post: PropTypes.object.isRequired
-}
+    getPost: PropTypes.func.isRequired,
+    post: PropTypes.object.isRequired,
+};
 
 const mapStateToProps = state => ({
-  post: state.post
-})
-export default connect(mapStateToProps, { getPost })(Post)
+    post: state.post,
+});
+
+export default connect(mapStateToProps, { getPost })(Post);
